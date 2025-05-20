d6a94e74089f2aef85c22bb279680116dfc18c4c
SangamShrestha01 <136991231+SangamShrestha01@users.noreply.github.com>
2025-05-20 19:22:40 +0545

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
+const express = require('express');
+const server = express();
 
-const express = require('express')
-const app = express()
-const port = 5500
+server.get('/', (request, response) => {
+  response.send('Hello World');
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello World! my name is sangam')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
